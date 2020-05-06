05c1ea1e0c231ca96338e0910c76ae37f48489e8
Jeff Sutch <jeff.sutch@acm.org>
2020-05-06 15:03:52 -0700

    various matplotlib animation examples

---
+++ celluloid_examples.py
@@ -0,0 +1,25 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from celluloid import Camera
+
+fig, axes = plt.subplots(2)
+camera = Camera(fig)
+t = np.linspace(0, 2 * np.pi, 128, endpoint=False)
+for i in t:
+    axes[0].plot(t, np.sin(t + i), color='blue')
+    axes[1].plot(t, np.sin(t - i), color='blue')
+    camera.snap()
+
+animation = camera.animate()
+animation.save('celluloid_subplots.gif', writer = 'imagemagick')
+
+
+# Writing legends with celluloid
+fig = plt.figure()
+camera = Camera(fig)
+for i in range(20):
+    t = plt.plot(range(i, i + 5))
+    plt.legend(t, [f'line {i}'])
+    camera.snap()
+animation = camera.animate()
+animation.save('celluloid_legends.gif', writer = 'imagemagick')
